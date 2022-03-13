1024f32d653709085089defe7906779dabadbfe0
Riasuu <101504665+Riasuu@users.noreply.github.com>
2022-03-13 19:48:49 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,16 @@
+function validate()
+(
+
+var username-docunent.getElenentById("username").value;
+var password-docunent.getilenentlyid("password").value;
+if(username=="admin"&s password=="user")
+{
+   alert("login succestully");
+    return false;
+}
+else
+{
+alert("login failed")
+}
+
+}
